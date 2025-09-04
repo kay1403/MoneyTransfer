8aab36e20dda72dc4170a1ba0eaa26df9a435ad8
kay1403 <angekoumba754@gmail.com>
2025-09-04 18:00:16 +0200

    Update urls.py

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import TransactionCreateView, TransactionListView
+from .views import (
+    TransactionCreateView,
+    TransactionListView,
+    TransactionConfirmView,
+    TransactionReceiptView
+)
 
 urlpatterns = [
     path('create/', TransactionCreateView.as_view(), name='create-transaction'),
     path('', TransactionListView.as_view(), name='list-transactions'),
-    path('receipt/<int:pk>/', TransactionReceiptView.as_view(), name='transaction-receipt'),
+    path('confirm/<int:pk>/', TransactionConfirmView.as_view(), name='confirm-transaction'),  # confirmation
+    path('receipt/<int:pk>/', TransactionReceiptView.as_view(), name='transaction-receipt'), # téléchargement PDF
 ]
