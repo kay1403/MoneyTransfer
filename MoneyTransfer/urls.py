0d591beb3b7d3dae7ec80108683b7ab6743e87d4
kay1403 <angekoumba754@gmail.com>
2025-09-04 16:20:50 +0200

    Update urls.py

--- MoneyTransfer/urls.py
+++ MoneyTransfer/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView, RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,7 @@ urlpatterns = [
     path('api/', RedirectView.as_view(url='/api/core/', permanent=False)),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+# ✅ Servir les fichiers uploadés en développement / production
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
