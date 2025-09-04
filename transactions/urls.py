e9ee8e4934ebe9de0a48f2d9f6aa495b0c77475d
kay1403 <angekoumba754@gmail.com>
2025-09-04 16:43:37 +0200

    Update urls.py

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,4 +4,5 @@ from .views import TransactionCreateView, TransactionListView
 urlpatterns = [
     path('create/', TransactionCreateView.as_view(), name='create-transaction'),
     path('', TransactionListView.as_view(), name='list-transactions'),
+    path('receipt/<int:pk>/', TransactionReceiptView.as_view(), name='transaction-receipt'),
 ]
