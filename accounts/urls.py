65ffb6ca00549dcc73b99a4b01a342b096ed938b
kay1403 <angekoumba754@gmail.com>
2025-09-04 15:02:50 +0200

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,13 +3,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, UserListView
 
 urlpatterns = [
-    # POST {username, password} -> { access, refresh }
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Inscription
     path('register/', RegisterView.as_view(), name='register'),
-
-    # (optionnel) liste des users (admin)
     path('users/', UserListView.as_view(), name='user-list'),
 ]
