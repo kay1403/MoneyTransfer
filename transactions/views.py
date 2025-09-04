0c190f425f8f1a9808645af779c8d88e4e6d0f73
kay1403 <angekoumba754@gmail.com>
2025-09-04 15:04:17 +0200

    Update views.py

--- transactions/views.py
+++ transactions/views.py
@@ -1,14 +1,18 @@
 from rest_framework import generics, permissions
+from rest_framework.parsers import MultiPartParser, FormParser
 from .models import Transaction
 from .serializers import TransactionSerializer
 
 class TransactionCreateView(generics.CreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    parser_classes = (permissions.AllowAny,)  # Pour multipart/form-data
+    parser_classes = [MultiPartParser, FormParser]
     permission_classes = [permissions.IsAuthenticated]
 
-class TransactionListView(generics.ListCreateAPIView):  # <-- ListCreateAPIView au lieu de ListAPIView
-    queryset = Transaction.objects.all()
+class TransactionListView(generics.ListAPIView):
     serializer_class = TransactionSerializer
-    # Ajoutez permissions si nécessaire
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(sender=user) | Transaction.objects.filter(receiver=user)
